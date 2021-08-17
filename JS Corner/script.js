9143f59bd913b4fed72aea6c40347b48cd3665bb
himangshuishere <dehimangshu2020@outlook.com>
2021-08-17 15:33:34 +0530

    Added two files in the js corner folder

---
+++ JS Corner/script.js
@@ -0,0 +1,12 @@
+console.time("Loading time") // Starting point of time function
+console.log("Hello Himangshu!"); // String printing in the console
+console.log(4+3598)// Sum printing in the console
+console.log(true) // Boolean printing in the console
+console.log([34, "String", "Himangshu de", "himangshuishere", 65, 6.3, 50]) //This is an array in JS
+console.log({main: "Himangshu", secondary: "De"}) //This is an JS object in JS
+console.table({main: "Himangshu", secondary: "De"}) //Tabulation of Object in JS
+console.warn("This is a sample warning!") //This is an warning in JS
+console.error("This is a sample error!")
+console.timeEnd("Loading time") //Returns the loading time of the code from the line where console.time() was introduced
+// console.clear() //Clears the console
+console.assert(55<40, "Age > 40 is not possible!") //A sample assertion example... if fails then throws an error.
